205404de6d1ffda0557683beeba3cc69b398526b
ttreit <14933277+ttreit@users.noreply.github.com>
2020-07-20 13:39:25 -0700

    Remove database connect from here (its in database/index.js)

--- server/index.js
+++ server/index.js
@@ -7,32 +7,11 @@ const logger = require('./middleware/logger');
 const app = express();
 const port = 1128;
 
-mongoose.connect('mongodb://localhost/test', {useMongoClient: true});
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'conenction error:'));
-db.once('open', () => {
-  console.log('Connected to DB');
-  const repoSchema = new mongoose.Schema({
-
-  })
-
-}); //end db callback
-
-
-
 app.use (express.static(path.join(__dirname, '../client/dist/')));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(logger);
-
-app.use((req, res) =>  {
-  console.log(req.method, '---->', req.url);
-})
-
-app.get('/', (req, res) => (
-  res.sendFile(index.html)
-));
 
 app.post('/repos', function (req, res) {
   // TODO - your code here!
