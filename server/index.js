e23acacf1856846c201c7a57c4f19923959b242d
ttreit <14933277+ttreit@users.noreply.github.com>
2020-07-20 14:53:06 -0700

    Change order of middleware so Logger runs

--- server/index.js
+++ server/index.js
@@ -7,11 +7,11 @@ const logger = require('./middleware/logger');
 const app = express();
 const port = 1128;
 
+app.use(logger);
+
 app.use (express.static(path.join(__dirname, '../client/dist/')));
 
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(logger);
 
 app.post('/repos', function (req, res) {
   // TODO - your code here!
