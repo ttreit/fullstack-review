e41cd9dac8199da54ba96639b025c936599673f6
ttreit <14933277+ttreit@users.noreply.github.com>
2020-07-22 13:17:24 -0700

    Create helper function for GH api requests

--- helpers/github.js
+++ helpers/github.js
@@ -1,20 +1,39 @@
-const axios = require('axios');
+const request = require('request');
 const config = require('../config.js');
+const path = require('path');
 
-let getReposByUsername = (/* TODO */) => {
-  // TODO - Use the axios module to request repos for a specific
-  // user from the github API
 
-  // The options object has been provided to help you out, 
-  // but you'll have to fill in the URL
+let getReposByUsername = (userName) => {
+  //const gitHubUrl = path.join('https://api.github.com/users/', userName, 'repos');
+  //console.log('URL', gitHubUrl);
+
+  //TODO Understand why I could not get it to work with PATH but could get it to work with a template literal
+
   let options = {
-    url: 'FILL ME IN',
+    //url: gitHubUrl,
+    url: `https://api.github.com/users/${userName}/repos`,
     headers: {
       'User-Agent': 'request',
       'Authorization': `token ${config.TOKEN}`
     }
   };
 
+
+  function callback(error, response, body) {
+    console.log('****', response);
+    if (!error) {
+      const info = JSON.parse(body);
+      console.log(info.stargazers_count + 'Stars');
+      //cb(info) (add callback as second parameter to getReposByUsername)
+    }
+  }
+
+  request(options, callback);
+
+
 }
 
 module.exports.getReposByUsername = getReposByUsername;
+
+
+
