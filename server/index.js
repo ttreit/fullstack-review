88621b42829033583b037ad8fc93b74b5a031758
ttreit <14933277+ttreit@users.noreply.github.com>
2020-07-22 18:24:57 -0700

    Add requirements for save and github

--- server/index.js
+++ server/index.js
@@ -3,19 +3,20 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const logger = require('./middleware/logger');
+const save = require('../database/index');
+const gh = require('../helpers/github');
 
-
-
-const app = express();
 const port = 1128;
+const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser());
 
 app.use(logger);
 
 app.use (express.static(path.join(__dirname, '../client/dist/')));
 
 app.post('/repos', function (req, res) {
+
 
 
   // This route should take the github username provided
