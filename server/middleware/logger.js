ccd4aa0a27c70080b39a446d099c499528b55707
ttreit <14933277+ttreit@users.noreply.github.com>
2020-07-22 18:38:54 -0700

    Add username string to req.body.username

--- server/middleware/logger.js
+++ server/middleware/logger.js
@@ -1,6 +1,17 @@
 const logger = function(req, res, next) {
   console.log(req.method, '---->', req.url);
   console.log('BODY****', req.body);
+
+  //get property
+  const getProperty = function() {
+    for (let key in req.body) {
+      return key;
+    }
+  }
+
+  req.body.userName = getProperty();
+  console.log('body.username', req.body.userName);
+
   next()
 }
 
